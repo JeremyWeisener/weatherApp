64e70b63abf4f513a27a44b5ecf980b3b2a197d2
JeremyWeisener <jweisener@gmail.com>
2016-09-22 21:46:12 -0400

    Update stuff.js

--- stuff.js
+++ stuff.js
@@ -88,7 +88,7 @@ function weatherCallBack(data){
         $('#icon').html("<i class=\"wi wi-day-fog\"></i>");
     }
     else if(weatherCode==800){
-        $('#icon').html("<i class=\"wi wi-night-clear\"></i>");
+        $('#icon').html("<i class=\"wi wi-day-sunny\"></i>");
     }
     else if(800<weatherCode<900){
         $('#icon').html("<i class=\"wi wi-day-cloudy\"></i>");
